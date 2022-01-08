7b792a79aaacc435bcc02385d87f21a8780490d5
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-08 00:13:33 -0500

    add product totals for each category to admin page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
   
   def show
-    @products_count = Product.count
-    @categories_count = Category.count
+    @products = Product.all
+    @categories = Category.all
   end
 end

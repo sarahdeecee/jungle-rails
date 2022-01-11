c2233e5a3c1dd4c5e619b34d210001a813628641
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-11 02:59:24 -0500

    implement categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,31 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: "Jungle", password: "book"
+  
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = Category.new(category_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
 end
+  

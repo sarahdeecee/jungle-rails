ed7d7313c382ccdafd3e90227efa7b41c3c93164
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-11 21:47:01 -0500

    update app controller, add user session to nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
-
+  #cart management
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
@@ -20,13 +20,22 @@ class ApplicationController < ActionController::Base
   end
   helper_method :cart_subtotal_cents
 
-
   def update_cart(new_cart)
     cookies[:cart] = {
       value: JSON.generate(new_cart),
       expires: 10.days.from_now
     }
     cookies[:cart]
+  end
+
+  #user session management
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
   end
 
 end

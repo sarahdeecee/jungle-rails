a1a71267af4a301c3332b5c585c5affa963d356c
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-11 22:33:06 -0500

    update registration page, sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to '/'
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/', notice: 'Account registered!'
     else
-      redirect_to '/register'
+      render :new
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
 
   get '/register' => 'users#new'
   post '/users' => 'users#create'
+  get '/users' => 'products#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

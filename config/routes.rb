699ed0cf52e6385485b96257e793da5a93e45469
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-11 20:47:12 -0500

    add user controller, add routes for user registration

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,11 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
-
+  
   get 'about' => 'about#index'
+
+  get '/register' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

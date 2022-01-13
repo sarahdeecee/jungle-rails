e82328ac0e90daf66f5b997406e17d30dad1daf5
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-12 20:26:31 -0500

    add before to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
+    before do
+      @user = User.new(first_name: "First", last_name: "Last", email: "1234@test.com", password_digest: "123")
+    end
+    
   end
 end

7dbab76afdbba220419a72ffbfda62e2179bd797
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-13 02:30:24 -0500

    add spec for product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can navigate from the home page to the product detail page by clicking on a product" do
+    # ACT
+    visit root_path
+    first('.product').first(:link).click
+
+    # DEBUG
+    # save_and_open_screenshot
+
+    #VERIFY
+    expect(page).to have_css '.products-show', count: 1
+  end
+
+end

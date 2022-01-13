fa1e85d312ecc3142e8506311a4902925668f1d2
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-13 00:17:37 -0500

    update user spec, update user model with authenticate_with_credentials, update sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, :password_confirmation, length: { minimum: 8 }
 
   def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.downcase.strip)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe User, type: :model do
         first_name: "First",
         last_name: "Last",
         email: "1234@test.com",
-        password: "123",
-        password_confirmation: "123"
+        password: "12345678",
+        password_confirmation: "12345678"
       )
     end
 
@@ -16,36 +16,61 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
     end
     
-    it 'creates a user even if there are extra spaces around the email address' do
-      # @user.email = "  1234@test.com "
-      @user.email = " "
-      expect(@user).to be_valid
-    end
-    
-    it 'creates a user when the email address has the wrong case' do
-      # @user.email = "1234@TEST.coM"
-      @user.email = ""
-      expect(@user).to be_valid
-    end
-    
     it 'is invalid when password and password_confirmation do not match' do
-      # @user.password = "456"
+      @user.password = "456"
       expect(@user).not_to be_valid
     end
     
     it 'is invalid when password is not given' do
-      # @user.password = nil
+      @user.password = nil
       expect(@user).not_to be_valid
     end
     
     it 'is invalid when password_confirmation is not given' do
-      # @user.password_confirmation = nil
+      @user.password_confirmation = nil
       expect(@user).not_to be_valid
     end
     
-    it 'is invalid when password is less than the minimum length' do
-      # @user.password_confirmation = nil
+    it 'is invalid when email is not given' do
+      @user.email = nil
       expect(@user).not_to be_valid
+    end
+
+    it 'is invalid when first name is not given' do
+      @user.first_name = nil
+      expect(@user).not_to be_valid
+    end
+
+    it 'is invalid when last name is not given' do
+      @user.last_name = nil
+      expect(@user).not_to be_valid
+    end
+
+    it 'is invalid when email is not unique (non-case-sensitive)' do
+      @user.save
+      @new_user = User.new(
+        first_name: "FirstNew",
+        last_name: "LastNew",
+        email: "1234@TEST.com",
+        password: "12345678",
+        password_confirmation: "12345678"
+      )
+      expect(@user).to be_valid
+      expect(@new_user).not_to be_valid
+    end
+    
+    it 'is valid when password is more than the minimum length (8 char)' do
+      @user.password = '123456789'
+      @user.password_confirmation = '123456789'
+      valid_length = (@user.password).length >= 8 && (@user.password_confirmation).length >= 8
+      expect(valid_length).to be_truthy
+    end
+
+    it 'is invalid when password is less than the minimum length (8 char)' do
+      @user.password = '1234567'
+      @user.password_confirmation = '1234567'
+      valid_length = (@user.password).length >= 8 && (@user.password_confirmation).length >= 8
+      expect(valid_length).to be_falsey
     end
   end
 
@@ -55,38 +80,34 @@ RSpec.describe User, type: :model do
         first_name: "First",
         last_name: "Last",
         email: "1234@test.com",
-        password: "123",
-        password_confirmation: "123"
+        password: "12345678",
+        password_confirmation: "12345678"
       )
-    end
-    
-    it 'logs the user in when the email and password are correct' do
       @user.save
-      @login_user = User.authenticate_with_credentials('1234@test.com', '123')
+    end
+
+    it 'logs the user in when the email and password are correct' do
+      @login_user = User.authenticate_with_credentials('1234@test.com', '12345678')
       expect(@user).to eq(@login_user)
     end
     
     it 'does not log the user in when the email is incorrect' do
-      @user.save
-      @login_user = User.authenticate_with_credentials('1234@test.com', '123')
+      @login_user = User.authenticate_with_credentials('1234@test.com', '12345678')
       expect(@user).to eq(@login_user)
     end
     
     it 'does not log the user in when the password is incorrect' do
-      @user.save
-      @login_user = User.authenticate_with_credentials('1234@test.com', '123')
+      @login_user = User.authenticate_with_credentials('1234@test.com', '12345678')
       expect(@user).to eq(@login_user)
     end
     
     it 'logs the user in when the email has extra whitespace around it' do
-      @user.save
-      @login_user = User.authenticate_with_credentials('1234@test.com', '123')
+      @login_user = User.authenticate_with_credentials('  1234@test.com ', '12345678')
       expect(@user).to eq(@login_user)
     end
     
     it 'logs the user in when the email has the wrong case' do
-      @user.save
-      @login_user = User.authenticate_with_credentials('1234@test.com', '123')
+      @login_user = User.authenticate_with_credentials('1234@TEST.com', '12345678')
       expect(@user).to eq(@login_user)
     end
 

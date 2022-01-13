a5641ba34a76e7c0439fc32bd78ffa73b4798096
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-01-12 20:03:58 -0500

    expand examples for product spec to include error messages

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -12,24 +12,29 @@ RSpec.describe Product, type: :model do
       expect(@product).to be_valid
     end
 
-    it 'is invalid when creating a product without a name' do
+    it 'is invalid and contains the appropriate error when creating a product without a name' do
       @product.name = nil
       expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
-    it 'is invalid when creating a product without a price' do
+
+    it 'is invalid and contains the appropriate error when creating a product without a price' do
       @product.price_cents = nil
       expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Price cents can't be blank")
     end
-    it 'is invalid when creating a product without a quantity' do
+
+    it 'is invalid and contains the appropriate error when creating a product without a quantity' do
       @product.quantity = nil
       expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
-    it 'is invalid when creating a product without a category' do
+
+    it 'is invalid and contains the appropriate error when creating a product without a category' do
       @product.category = nil
       expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
-
-
       
   end
 end
